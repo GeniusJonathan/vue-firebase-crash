7b66e849a4af3981747d2a09077e141386747d11
jykhli@gmail.com <jykhli@gmail.com>
2019-05-02 23:43:08 +0200

    Completed Dashboard, ViewEmployee pages

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCqELih6BYM21bGAMWEWdDGQx0jHRqZWLM",
+    authDomain: "vue-firebase-crash.firebaseapp.com",
+    databaseURL: "https://vue-firebase-crash.firebaseio.com",
+    projectId: "vue-firebase-crash",
+    storageBucket: "vue-firebase-crash.appspot.com",
+    messagingSenderId: "213569119618"
+}
