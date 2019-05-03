c46eb286ad53b9293dd6182f6c069ce40483aec4
GeniusJonathan <jykhli@gmail.com>
2019-05-03 21:05:20 +0200

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyCqELih6BYM21bGAMWEWdDGQx0jHRqZWLM",
-    authDomain: "vue-firebase-crash.firebaseapp.com",
-    databaseURL: "https://vue-firebase-crash.firebaseio.com",
-    projectId: "vue-firebase-crash",
-    storageBucket: "vue-firebase-crash.appspot.com",
-    messagingSenderId: "213569119618"
-}
