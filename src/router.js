71197da667a9f956855a78ec786d4a3faf93dcac
jykhli@gmail.com <jykhli@gmail.com>
2019-05-13 00:41:06 +0200

    added authentication to project

--- src/router.js
+++ src/router.js
@@ -4,30 +4,100 @@ import Dashboard from "./components/Dashboard.vue";
 import EditEmployee from "./components/EditEmployee.vue";
 import NewEmployee from "./components/NewEmployee.vue";
 import ViewEmployee from "./components/ViewEmployee.vue";
+import Login from "./components/Login.vue";
+import Register from "./components/Register.vue";
+import firebase from "firebase";
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   routes: [
     {
-      path: "/",
-      name: "dashboard",
-      component: Dashboard
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: "/new",
-      name: "new-employee",
-      component: NewEmployee
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
-      path: "/edit/:employee_id",
-      name: "edit-employee",
-      component: EditEmployee
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
-      path: "/:employee_id",
-      name: "view-employee",
-      component: ViewEmployee
+      path: '/new',
+      name: 'new-employee',
+      component: NewEmployee,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/edit/:employee_id',
+      name: 'edit-employee',
+      component: EditEmployee,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/:employee_id',
+      name: 'view-employee',
+      component: ViewEmployee,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
+
+// Nav Guard
+router.beforeEach((to, from, next) => {
+  // Check for requiresAuth guard
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Check if NO logged user
+    if (!firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    // Check if NO logged user
+    if (firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: '/',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else {
+    // Proceed to route
+    next();
+  }
+});
+
+export default router;
